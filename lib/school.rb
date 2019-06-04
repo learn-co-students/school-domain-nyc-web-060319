7d15aa7a7c21964e2b05cbe64f9cb5bfa78478fa
CindyChenx <chen_cx0128@hotmail.com>
2019-06-04 17:37:02 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+class School
+
+    attr_accessir :roster
+
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def roster
+        hash = {}
+    end
+
+    def add_student(name, grade)
+        @name = name
+        @grade = grade
+       if @roster.has_key?(grade)
+            @roster[grade] = []
+            @roster[grade] << name
+       end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+
+    end
+end
